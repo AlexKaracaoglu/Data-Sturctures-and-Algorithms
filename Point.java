7d45e634ed2a4cef41ea08ab12e5676be605de7d
AlexKaracaoglu <31391932+AlexKaracaoglu@users.noreply.github.com>
2017-10-10 20:09:31 -0400

    Delete Point.java

--- Point.java
+++
@@ -1,8 +0,0 @@
-public interface Point {    
-    public double getX();
-    public double getY();
-    public Point move(double dx, double dy);
-
-    public String toString();
-    public boolean equals(Point other);
-}
